31c7d8a4522549cb7a6120da8dfe71e865255dfb
DmitryInTheDark <bosy.dmitry1@mail.ru>
2025-05-19 22:59:55 +0300

    Небольшие исправления

--- data/src/main/java/com/example/data/remotedatasource/apiresponseimplementation/ApiResponseImplementation.kt
+++ data/src/main/java/com/example/data/remotedatasource/apiresponseimplementation/ApiResponseImplementation.kt
@@ -5,7 +5,7 @@ import com.example.data.remotedatasource.model.ApiResponse
 import com.example.data.remotedatasource.model.Post
 
 class ApiResponseImplementation(private val api: ApiResponseInterface = RetrofitBuilder().api) {
-    suspend fun getAllPosts(page: Int): List<Post>{
+    suspend fun getPostsForUser(page: Int): List<Post>{
         val firstList = api.getPostsFromPage(page).data
         val secondList = api.getPostsFromPage(page + 1).data
         val finalList = firstList + secondList
--- data/src/main/java/com/example/data/remotedatasource/apiresponseinterface/ApiResponseInterface.kt
+++ data/src/main/java/com/example/data/remotedatasource/apiresponseinterface/ApiResponseInterface.kt
@@ -1,10 +1,15 @@
 package com.example.data.remotedatasource.apiresponseinterface
 
 import com.example.data.remotedatasource.model.ApiResponse
+import com.example.data.remotedatasource.model.Post
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiResponseInterface{
+
     @GET("posts")
     suspend fun getPostsFromPage(@Query("page") page: Int): ApiResponse
+
+    @GET("posts")
+    suspend fun getPosts(): ApiResponse
 }
